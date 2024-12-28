5f35dfa22eee7c4e554e89f0a17f3768db303a22
Dmmsys <46485742+Bonainifo@users.noreply.github.com>
2024-12-28 13:17:38 +0800

    Add files via upload

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-####################################################################################
-#                                                                                  #
-# Copyright (C) 2016~2025 Dmmsys <guoyihot@outlook.com> Guobower<124358678@qq.com>##
-# All Rights Reserved                                                              #
-#  Shanghai Hengzao                                                                #
-####################################################################################
 
 from . import documents
+from . import home
+from . import portal
